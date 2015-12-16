a590b2e9bdb9dbeb1912834f04f7d2fd3f1ba094
michaelpetty <michael.petty@gmail.com>
2015-12-15 21:36:58 -0800

    disable microphone; hide stop

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var microphone: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,9 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+    }
 
     @IBAction func startRecording(sender: UIButton) {
         recordingLabel.hidden = false
+        stopButton.hidden = false
+        microphone.enabled = false
         print("start recording")
     }
 }
